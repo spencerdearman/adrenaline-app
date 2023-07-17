300532cb7727eb5c48974dd648b0b168c636b208
Logan Sherwin <lsherwin10@gmail.com>
2023-07-17 14:33:11 -0400

    Clean up unused variables/HTMLParser instances

--- Adrenaline/Adrenaline/Views/Util/SwiftUIWebView.swift
+++ Adrenaline/Adrenaline/Views/Util/SwiftUIWebView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import WebKit
+import SwiftSoup
 
 struct SwiftUIWebView: View {
     @Binding var firstName: String
@@ -29,7 +30,6 @@ struct SwiftUIWebView: View {
 }
 
 struct WebView: UIViewRepresentable {
-    let htmlParser: HTMLParser = HTMLParser()
     @Binding var request: String
     @Binding var parsedHTML: String
     @Binding var parsedLinks: DiverProfileRecords
@@ -95,6 +95,30 @@ struct WebView: UIViewRepresentable {
             self._timedOut = timedOut
         }
         
+        private func getRecords(_ html: String) -> DiverProfileRecords {
+            let leadingLink: String = "https://secure.meetcontrol.com/divemeets/system/"
+            var result: DiverProfileRecords = [:]
+            do {
+                let document: Document = try SwiftSoup.parse(html)
+                guard let body = document.body() else {
+                    return [:]
+                }
+                let content = try body.getElementById("dm_content")
+                let links = try content?.getElementsByClass("showresults").select("a")
+                try links?.forEach({ l in
+                    // Adds an empty list value to a new key
+                    if !result.keys.contains(try l.text()) {
+                        result[try l.text()] = []
+                    }
+                    result[try l.text()]!.append(try leadingLink + l.attr("href"))
+                })
+            }
+            catch {
+                print("Parsing records failed")
+            }
+            return result
+        }
+        
         func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
             let js = "document.getElementById('first').value = '\(firstName)';"
             + "document.getElementById('last').value = '\(lastName)'"
@@ -114,7 +138,7 @@ struct WebView: UIViewRepresentable {
                     [weak self] result, error in
                     guard let html = result as? String, error == nil else { return }
                     self?.parsedHTML = html
-                    self?.parsedLinks = (self?.htmlParser.getRecords(html)) ?? [:]
+                    self?.parsedLinks = (self?.getRecords(html)) ?? [:]
                     self?.linksParsed = true
                 }
             }
