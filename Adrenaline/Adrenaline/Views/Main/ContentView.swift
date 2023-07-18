0df12e887acc9803900559af37357df1c2314060
Logan Sherwin <lsherwin10@gmail.com>
2023-07-18 02:02:03 -0400

    Add test view to tools menu

--- Adrenaline/Adrenaline/Views/Main/ContentView.swift
+++ Adrenaline/Adrenaline/Views/Main/ContentView.swift
@@ -74,8 +74,9 @@ struct ContentView: View {
 //                                            FinishedLiveResultsView(link: "https://secure.meetcontrol.com/divemeets/system/livestats.php?event=stats-9050-770-9-Finished")
                                         //}
                                         //.navigationViewStyle(StackNavigationViewStyle())
-                                        ToolsMenu()
+//                                        ToolsMenu()
                                          //SearchColorfulView()
+                                        FirstOpenView()
                                     case .magnifyingglass:
                                         SearchView(isIndexingMeets: $isIndexingMeets)
                                     case .person:
