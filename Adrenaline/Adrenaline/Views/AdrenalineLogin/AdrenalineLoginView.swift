60f88e6ccf2f866414f7137fb0eb3034ced675f3
spencerdearman <121642493+spencerdearman@users.noreply.github.com>
2023-08-06 14:12:22 -0400

    Fixing one issue, still having environment issue

--- Adrenaline/Adrenaline/Views/AdrenalineLogin/AdrenalineLoginView.swift
+++ Adrenaline/Adrenaline/Views/AdrenalineLogin/AdrenalineLoginView.swift
@@ -58,9 +58,6 @@ struct AdrenalineLoginView: View {
                     // https://developer.apple.com/forums/thread/693137
                     .navigationViewStyle(.stack)
                 }
-                .navigationViewStyle(StackNavigationViewStyle())
-                // Bless the person that figured this out:
-                // https://developer.apple.com/forums/thread/693137
             }
             // Clears email and password on Logout press back to this page
             .onChange(of: loginSuccessful) { newValue in
