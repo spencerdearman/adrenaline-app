e758340fb1b9d3bdde8a1b1dee0b49a5cb235326
Logan Sherwin <lsherwin10@gmail.com>
2023-10-23 00:07:46 -0700

    Update cloudfront constants and add bucket constants

---
+++ Adrenaline/Adrenaline/Views/AdrenalineProfile/PostProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  PostProfileView.swift
+//  Adrenaline
+//
+//  Created by Logan Sherwin on 10/22/23.
+//
+
+import SwiftUI
+
+struct PostProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostProfileView()
+}
