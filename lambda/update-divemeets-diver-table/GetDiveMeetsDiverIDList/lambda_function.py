42f3f6b04a8e1b8a32177cbc01b6f86520eda3ce
Logan Sherwin <lsherwin10@gmail.com>
2023-12-02 19:14:59 -0800

    Update lambdas with final code and remove unused ones

--- lambda/update-divemeets-diver-table/GetDiveMeetsDiverIDList/lambda_function.py
+++ lambda/update-divemeets-diver-table/GetDiveMeetsDiverIDList/lambda_function.py
@@ -1,5 +1,10 @@
-# This updates the DiveMeetsDiver DynamoDB table with whatever DiveMeets id list
-# is provided as a yyyy-MM-dd in the event dictionary, e.g. { "input_date": "2023-11-30" }
+# This generates a list of DiveMeets ids that fall in high school age range or
+# graduation years to update the Adrenaline rankings list. It saves these lists
+# as a CSV titled yyyy-MM-dd.csv for the given run date.
+# This should execute on a weekly schedule on Wedesndays at 9AM ET (UTC-5).
+# If this is being run manually, you must provide a dictionary like
+# this: { "start_index": "25000", "end_index": "150000" },
+# where the ints can be changed to update the DiveMeets id range that will be parsed.
 # ID list files are saved here: s3://adrenalinexxxxx153503-main/public/update-divemeets-diver-table/divemeets-divers-lists/
 # This lambda runs an EC2 instance and logs to this CloudWatch log group: /aws/ec2/update-divemeets-diver-table
 
@@ -7,6 +12,8 @@ import boto3
 import json
 import time
 import os
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 region = "us-east-1"
 ec2_id = os.environ["ec2_id"]
@@ -30,47 +37,45 @@ def lambda_handler(event, context):
         ]
     )
 
-    print(f"{event=}")
-    assert "input_date" in event
-    input_date = event["input_date"]
+    eventJson = json.dumps(event).replace('"', '\\"')
+    print(f"{event=}, JSON: {eventJson}")
 
     script = f"""
 #!/bin/bash
+python_script="from script import run; run({eventJson}, \\"{log_group_name}\\")"
 echo "Copying starter code..."
+
 cd /home/ec2-user
 
 # Clean up any leftover resources that were missed from a previous failure
 rm -r update-divemeets-diver-table && {{ echo "Removed old update-divemeets-diver-table folder before continuing..."; }} || {{ echo "No old update-divemeets-diver-table found, continuing..."; }}
 
 # Copy necessary scripts to the filesystem
+aws s3 cp s3://adrenalinexxxxx153503-main/public/update-divemeets-diver-table/script.py update-divemeets-diver-table/script.py && \
 aws s3 cp --recursive s3://adrenalinexxxxx153503-main/public/update-divemeets-diver-table/update-dynamodb/ update-divemeets-diver-table/update-dynamodb/ && \
 cd update-divemeets-diver-table || {{ echo "Failed to copy data from S3."; exit 1; }}
 
 # Set up python virtual environment
-echo "Creating virtual environment..."
+echo "Creating virtual environment..." && \
 sudo python3 -m venv venv && \
 sudo chmod -R a+rwx venv && \
 source venv/bin/activate && \
 pip install --upgrade pip && \
 pip install requests-futures boto3 bs4 html5lib simplejson || {{ echo "Failed to set up python virtual environment."; exit 1; }}
 
-# Get ids script from S3
-echo "Copying ids from S3..."
-aws s3 cp "s3://adrenalinexxxxx153503-main/public/update-divemeets-diver-table/divemeets-divers-lists/{input_date}.csv" ids.csv && \
-cd update-dynamodb || {{ echo "Failed to get ids file from S3."; exit 1; }}
-
 # Initiate python script
-python_script="from driver import run; run(\\"../ids.csv\\", \\"{log_group_name}\\")"
-echo "Adding python run script..."
-echo "$python_script" > start.py
-echo "Running python script..."                                              
+echo "Adding python run script..." && \
+echo "$python_script" > start.py && \
+echo "Running script..." && \
 python -u start.py && \
-cd .. && \
-echo "Script completed, deactivating virtual environment..."  && \
-# Post metric showing successful run and did not time out
-aws cloudwatch put-metric-data --region us-east-1 --namespace UpdateDiveMeetsDiverTable --metric-name StateMachineFailures --unit Count --value 0.0 || {{ echo "Python script failed to execute successfully."; }}
+echo "Script completed successfully" && \
+echo "Copying to S3..." && \
+date=$(date -d "-5 hours" +%F) && \
+aws s3 cp ids.csv "s3://adrenalinexxxxx153503-main/public/update-divemeets-diver-table/divemeets-divers-lists/$date.csv" || {{ echo "Python script failed to execute successfully."; exit 1; }}
 
-
+# Clean up resources
+cd ..
+echo "Deactivating virtual environment..."
 deactivate && \
 cd .. && \
 echo "Removing update-divemeets-diver-table folder..." && \
@@ -95,7 +100,7 @@ echo "Completed."
             print("Send succeeded")
             break
         except Exception as exc:
-            print(exc)
+            print(repr(exc))
             print("Failed response:", response)
             response = None
             print("send command failed, waiting 5 seconds before retrying...")
@@ -104,3 +109,11 @@ echo "Completed."
     if response is not None:
         # See the command run on the target instance Ids
         print("Response:", response["Command"]["Parameters"]["commands"])
+
+        return {
+            "input_date": datetime.now()
+            .astimezone(ZoneInfo("America/New_York"))
+            .strftime("%Y-%m-%d")
+        }
+
+    return {}
