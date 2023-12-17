54f149fc300a9c9d5e750f30c257b8e48661b70a
spencerdearman <121642493+spencerdearman@users.noreply.github.com>
2023-12-17 11:29:09 -0500

    Finally starting to look less horrendous

---
+++ Adrenaline/Adrenaline/Views/Overlays/MeetsBar.swift
@@ -0,0 +1,18 @@
+//
+//  MeetsBar.swift
+//  Adrenaline
+//
+//  Created by Spencer Dearman on 12/17/23.
+//
+
+import SwiftUI
+
+struct MeetsBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeetsBar()
+}
