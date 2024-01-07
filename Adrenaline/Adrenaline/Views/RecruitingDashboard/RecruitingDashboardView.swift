451e880d1ec941b29519cf15eac6d74500ed9aec
Logan Sherwin <lsherwin10@gmail.com>
2024-01-07 11:07:58 -0800

    Remove judging tab from coach profile

---
+++ Adrenaline/Adrenaline/Views/RecruitingDashboard/RecruitingDashboardView.swift
@@ -0,0 +1,18 @@
+//
+//  RecruitingDashboardView.swift
+//  Adrenaline
+//
+//  Created by Logan Sherwin on 1/7/24.
+//
+
+import SwiftUI
+
+struct RecruitingDashboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecruitingDashboardView()
+}
