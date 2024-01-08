2c93ff19c3f3b6cd7631c1e0232a04918daa8b76
Logan Sherwin <lsherwin10@gmail.com>
2024-01-07 17:52:29 -0800

    Add mechanism for switching the displayed sheet for expanding divers and results

---
+++ Adrenaline/Adrenaline/Views/RecruitingDashboard/ExpandedDiversView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpandedDiversView.swift
+//  Adrenaline
+//
+//  Created by Logan Sherwin on 1/7/24.
+//
+
+import SwiftUI
+
+struct ExpandedDiversView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpandedDiversView()
+}
