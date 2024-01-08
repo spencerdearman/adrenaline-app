d28720a765c343bb738a53ab2cb5e318d9bbf3b4
Logan Sherwin <lsherwin10@gmail.com>
2024-01-07 17:52:29 -0800

    Add mechanism for switching the displayed sheet for expanding divers and results

--- Adrenaline/Adrenaline/Views/RecruitingDashboard/RecruitingDashboardView.swift
+++ Adrenaline/Adrenaline/Views/RecruitingDashboard/RecruitingDashboardView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 import Amplify
 
+private enum Sheet {
+    case user
+    case divers
+    case results
+}
+
 struct RecruitingDashboardView: View {
     @EnvironmentObject private var appLogic: AppLogic
     @State private var contentHasScrolled: Bool = false
     @State private var feedModel: FeedModel = FeedModel()
     @State private var favorites: [NewUser] = []
     @State private var showSheet: Bool = false
+    @State private var selectedSheet: Sheet? = nil
     @State private var selectedUser: NewUser? = nil
     @Binding var newUser: NewUser?
     @Binding var showAccount: Bool
@@ -30,6 +37,13 @@ struct RecruitingDashboardView: View {
                         .fontWeight(.semibold)
                         .foregroundColor(.primary)
                     Spacer()
+                    Image(systemName: "chevron.right")
+                        .foregroundColor(.secondary)
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            selectedSheet = .divers
+                            showSheet = true
+                        }
                 }
                 
                 if favorites.count == 0 {
@@ -53,12 +67,13 @@ struct RecruitingDashboardView: View {
                                 .shadow(radius: 5)
                                 .onTapGesture {
                                     selectedUser = fav
+                                    selectedSheet = .user
                                     showSheet = true
                                 }
                             }
                         }
                     }
-            }
+                }
             }
             .padding(20)
             .background(.ultraThinMaterial)
@@ -66,11 +81,21 @@ struct RecruitingDashboardView: View {
             .backgroundStyle(cornerRadius: 30)
             .padding(20)
         }
+        .offset(y: 200)
         .sheet(isPresented: $showSheet) {
-            NavigationView {
-                if let user = selectedUser {
-                    AdrenalineProfileView(newUser: user)
-                }
+            switch selectedSheet {
+                case .user:
+                    NavigationView {
+                        if let user = selectedUser {
+                            AdrenalineProfileView(newUser: user)
+                        }
+                    }
+                case .divers:
+                    ExpandedDiversView()
+                case .results:
+                    Text("")
+                case nil:
+                    EmptyView()
             }
         }
         .overlay {
@@ -88,6 +113,7 @@ struct RecruitingDashboardView: View {
         .onChange(of: showSheet) {
             if !showSheet {
                 selectedUser = nil
+                selectedSheet = nil
             }
         }
         .onChange(of: appLogic.currentUserUpdated, initial: true) {
@@ -96,7 +122,7 @@ struct RecruitingDashboardView: View {
                     guard let favsIds = newUser?.favoritesIds else { return }
                     let favUsers = try await getAthleteUsersByFavoritesIds(ids: favsIds)
                     favorites = favUsers
-//                    print("Favorites:", favorites.map { $0.id })
+                    //                    print("Favorites:", favorites.map { $0.id })
                 }
             }
         }
