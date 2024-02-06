e1d7f7c392609ca88be2ab5f27979b9796974210
spencerdearman <121642493+spencerdearman@users.noreply.github.com>
2024-02-06 11:54:37 -0600

    Changing Home and Profile folder names

---
+++ react-app/src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Card, Heading } from '@aws-amplify/ui-react';
+
+const Home = () => {
+  return (
+    <Card>
+      <Heading level={1}>Home</Heading>
+    </Card>
+  );
+};
+
+
+export default Home;
