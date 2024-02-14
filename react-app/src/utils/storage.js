2b56e9d0d4e51d77a8de257f66adbeb874fc2173
Logan Sherwin <lsherwin10@gmail.com>
2024-02-14 09:09:22 -0800

    Add helper functions to get cloudfront urls

--- react-app/src/utils/storage.js
+++ react-app/src/utils/storage.js
@@ -1,13 +1,14 @@
-import { getUrl } from 'aws-amplify/storage';
+const CLOUDFRONT_STREAM_BASE_URL = 'https://d3mgzcs3lrwvom.cloudfront.net/';
+const CLOUDFRONT_IMAGE_BASE_URL = 'https://dc666cmbq88s6.cloudfront.net/';
 
-export async function getImageUrl(key) {
-  const urlResponse = await getUrl({
-    key
-  });
+export function getImageURL(user, imageId) {
+  return CLOUDFRONT_IMAGE_BASE_URL + `${user.email.replace('@', '%40')}/${imageId}.jpg`;
+}
 
-  if (urlResponse !== undefined) {
-    return urlResponse.url.href;
-  }
+export function getVideoHLSURL(user, videoId) {
+  return CLOUDFRONT_STREAM_BASE_URL + `${user.email.replace('@', '%40')}/${videoId}/output/HLS/${videoId}`;
+}
 
-  return undefined;
+export function getVideoThumbnailURL(user, videoId) {
+  return CLOUDFRONT_STREAM_BASE_URL + `${user.email.replace('@', '%40')}/${videoId}/output/Thumbnails/${videoId}.0000000.jpg`;
 }
