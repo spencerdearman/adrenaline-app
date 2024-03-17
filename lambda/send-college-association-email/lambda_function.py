79a925c5bec33b022c8550697a185974baffbcf4
Logan Sherwin <lsherwin10@gmail.com>
2024-03-17 11:04:33 -0700

    Reformat association request email to make it easier to invoke lambda

--- lambda/send-college-association-email/lambda_function.py
+++ lambda/send-college-association-email/lambda_function.py
@@ -12,7 +12,16 @@ def lambda_handler(event, context):
         filename = record["s3"]["object"]["key"]
         user_id, college_id = os.path.splitext(os.path.basename(filename))[0].split("_")
 
-        body = f'User "{user_id}" has requested to associate with the college "{college_id}". Match this college ID with the coach associated with this user to update their association.'
+        body = f"""A user has requested to associate with a college. Invoke the below lambda to update this association. If college ID is blank, provide an empty string in the "college_id" field of the input JSON.
+
+{{
+   "user_id": "{user_id}",
+   "college_id": "{college_id}"
+}}
+
+S3 Location: https://s3.console.aws.amazon.com/s3/buckets/adrenalinexxxxx153503-main?region=us-east-1&bucketType=general&prefix=public/college-association-requests/&showversions=false
+Lambda Function: https://us-east-1.console.aws.amazon.com/lambda/home?region=us-east-1#/functions/approve-college-association-request
+"""
 
         # Generate email response
         response = client.send_email(
