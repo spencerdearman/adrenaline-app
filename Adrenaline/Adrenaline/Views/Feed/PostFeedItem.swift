f896459dd4a65127c4d0e9a532b33bf9163fb2b7
spencerdearman <121642493+spencerdearman@users.noreply.github.com>
2024-03-17 23:33:36 -0500

    Much much closer, now the main issue is that once youve seen a video it is a little unpredictable, however, the posts do not play all the videos simulataneously.

--- Adrenaline/Adrenaline/Views/Feed/PostFeedItem.swift
+++ Adrenaline/Adrenaline/Views/Feed/PostFeedItem.swift
@@ -108,10 +108,12 @@ struct PostFeedItemCollapsedView: View {
                     .padding(.horizontal, 20)
                     .padding(.top, 10)
                     
-                    TabView(selection: $currentTab) {
-                        ForEach(mediaItems, id: \.id) { item in
-                            AnyView(item.view)
-                                .tag(item.id)
+                    GeometryReader { geometry in
+                        TabView(selection: $currentTab) {
+                            ForEach(mediaItems, id: \.id) { item in
+                                AnyView(item.view)
+                                    .tag(item.id)
+                            }
                         }
                     }
                     .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
--- Adrenaline/Adrenaline/Views/Video/BufferVideoPlayerView.swift
+++ Adrenaline/Adrenaline/Views/Video/BufferVideoPlayerView.swift
@@ -15,38 +15,58 @@ struct BufferVideoPlayerView: View {
     var isLooping: Bool
     var debugMode: Bool = false
     
+    // State to manage if the video player is centered
+    @State private var isCenterAlignedAndMidScreen = false
+    
     var body: some View {
-        ZStack {
-            VStack(alignment: .leading) {
-                if debugMode {
-                    Button("Change resolution") {
-                        withAnimation {
-                            showResolutions.toggle()
+        GeometryReader { fullGeometry in // Full view size for comparison
+            ZStack {
+                VStack(alignment: .leading) {
+                    if debugMode {
+                        Button("Change resolution") {
+                            withAnimation {
+                                showResolutions.toggle()
+                            }
+                        }
+                        .font(Font.body.bold())
+                    }
+                    
+                    VideoPlayer(player: videoPlayerVM.player) {
+                        if debugMode {
+                            Text(videoPlayerVM.namePlusResolution)
+                                .font(.subheadline)
+                                .foregroundColor(.white)
+                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                                .padding(8)
                         }
                     }
-                    .font(Font.body.bold())
-                }
-                    
-                VideoPlayer(player: videoPlayerVM.player) {
-                    if debugMode {
-                        Text(videoPlayerVM.namePlusResolution)
-                            .font(.subheadline)
-                            .foregroundColor(.white)
-                            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-                            .padding(8)
+                    .aspectRatio(videoPlayerVM.isVertical ? CGSize(width: 9, height: 16) : CGSize(width: 16, height: 9), contentMode: .fit)
+                    .onAppear {
+                        if playOnAppear {
+                            videoPlayerVM.player.play()
+                        }
                     }
+                    .onChange(of: isCenterAlignedAndMidScreen) { aligned in
+                        if aligned {
+                            videoPlayerVM.player.play()
+                        } else {
+                            videoPlayerVM.player.pause()
+                        }
+                    }
+                    .background(GeometryReader { geo in
+                        Color.clear.onAppear {
+                            // Calculate if the video player is centered
+                            let playerMidY = geo.frame(in: .global).midY
+                            let screenMidY = fullGeometry.size.height / 2
+                            let screenQuarterHeight = fullGeometry.size.height / 4
+                            
+                            isCenterAlignedAndMidScreen = abs(playerMidY - screenMidY) < screenQuarterHeight
+                        }
+                    })
                 }
-                .aspectRatio(videoPlayerVM.isVertical
-                             ? CGSize(width: 9, height: 16)
-                             : CGSize(width: 16, height: 9), contentMode: .fit)
                 .onAppear {
-                    if playOnAppear {
-                        videoPlayerVM.player.play()
-                    }
-                        
                     if isLooping {
-                        NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime,
-                                                               object: nil, queue: .main) { _ in
+                        NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: nil, queue: .main) { _ in
                             videoPlayerVM.player.seek(to: .zero)
                             videoPlayerVM.player.play()
                         }
@@ -54,37 +74,45 @@ struct BufferVideoPlayerView: View {
                 }
                 .onDisappear {
                     videoPlayerVM.player.pause()
-                }
-            }
-            
-            if debugMode && showResolutions {
-                VStack(spacing: 20) {
-                    Spacer()
-                    ForEach(Resolution.allCases) { resolution in
-                        Button(resolution.displayValue, action: {
-                            withAnimation {
-                                videoPlayerVM.selectedResolution = resolution
-                                showResolutions.toggle()
-                            }
-                        })
+                    if isLooping {
+                        NotificationCenter.default.removeObserver(self, name: .AVPlayerItemDidPlayToEndTime, object: nil)
                     }
-                    
-                    Button(action: {
-                        withAnimation {
-                            showResolutions.toggle()
-                        }
-                    }, label: {
-                        Image(systemName: "xmark.circle")
-                            .imageScale(.large)
-                    })
-                    .padding(.top)
-                    
-                    Spacer()
                 }
-                .frame(maxWidth: .infinity)
-                .background(.thinMaterial)
-                .transition(.move(edge: .bottom))
+                
+                if debugMode && showResolutions {
+                    resolutionPicker
+                }
             }
         }
+    }
+    
+    @ViewBuilder
+    private var resolutionPicker: some View {
+        VStack(spacing: 20) {
+            Spacer()
+            ForEach(Resolution.allCases) { resolution in
+                Button(resolution.displayValue, action: {
+                    withAnimation {
+                        videoPlayerVM.selectedResolution = resolution
+                        showResolutions.toggle()
+                    }
+                })
+            }
+            
+            Button(action: {
+                withAnimation {
+                    showResolutions.toggle()
+                }
+            }, label: {
+                Image(systemName: "xmark.circle")
+                    .imageScale(.large)
+            })
+            .padding(.top)
+            
+            Spacer()
+        }
+        .frame(maxWidth: .infinity)
+        .background(.thinMaterial)
+        .transition(.move(edge: .bottom))
     }
 }
