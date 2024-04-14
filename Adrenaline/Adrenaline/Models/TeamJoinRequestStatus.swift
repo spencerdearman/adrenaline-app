f82ec73525dae8b836717f28e41cfd6da15bb168
Logan Sherwin <lsherwin10@gmail.com>
2024-04-14 11:38:26 -0700

    Add requests with denial counts to TeamJoinRequestStatus enum

--- Adrenaline/Adrenaline/Models/TeamJoinRequestStatus.swift
+++ Adrenaline/Adrenaline/Models/TeamJoinRequestStatus.swift
@@ -4,6 +4,8 @@ import Foundation
 
 public enum TeamJoinRequestStatus: String, EnumPersistable {
   case requestedByAthlete = "REQUESTED_BY_ATHLETE"
+  case requestedByAthleteDeniedOnce = "REQUESTED_BY_ATHLETE_DENIED_ONCE"
+  case requestedByAthleteDeniedTwice = "REQUESTED_BY_ATHLETE_DENIED_TWICE"
   case requestedByCoach = "REQUESTED_BY_COACH"
   case approved = "APPROVED"
   case deniedByAthlete = "DENIED_BY_ATHLETE"
