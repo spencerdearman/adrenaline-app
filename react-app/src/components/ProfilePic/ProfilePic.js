a9dae70062e92671bb5f7c74ff2bb468f81a6e6a
Logan Sherwin <lsherwin10@gmail.com>
2024-04-27 13:41:51 -0700

    Fix profile pic loading on website after DOB change

--- react-app/src/components/ProfilePic/ProfilePic.js
+++ react-app/src/components/ProfilePic/ProfilePic.js
@@ -5,16 +5,23 @@ import defaultProfileIcon from '../../assets/images/defaultProfileIcon.png';
 
 const CLOUDFRONT_PROFILE_PICS_BASE_URL = 'https://dh68pb7jazk5m.cloudfront.net';
 
-export function getProfilePicUrl(id) {
-  return `${CLOUDFRONT_PROFILE_PICS_BASE_URL}/${id}.jpg`;
+export function getProfilePicUrl(id, firstName, lastName, dateOfBirth) {
+  if (!dateOfBirth) {
+    return '';
+  }
+  const date = dateOfBirth.substring(0, dateOfBirth.length - 1);
+  console.log(`${CLOUDFRONT_PROFILE_PICS_BASE_URL}/${id}_${firstName}_${lastName}_${date}.jpg`);
+  return `${CLOUDFRONT_PROFILE_PICS_BASE_URL}/${id}_${firstName}_${lastName}_${date}.jpg`;
 }
 
-export function ProfilePic ({ id }) {
+export function ProfilePic ({ id, firstName, lastName, dateOfBirth }) {
   return (
-    <Image src={getProfilePicUrl(id)} onError={({ currentTarget }) => {
-      currentTarget.onerror = null;
-      currentTarget.src = defaultProfileIcon;
-    }} />
+    <Image
+      src={getProfilePicUrl(id, firstName, lastName, dateOfBirth)}
+      onError={({ currentTarget }) => {
+        currentTarget.onerror = null;
+        currentTarget.src = defaultProfileIcon;
+      }} />
   );
 };
 
